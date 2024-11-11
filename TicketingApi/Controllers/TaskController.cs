10c1e6feb4776508d44f89de38b6cc7056f83426
Hunter Robb <hunter_robb@yahoo.com>
2024-11-11 15:02:33 -0600

    Added base methods for each

--- TicketingApi/Controllers/TaskController.cs
+++ TicketingApi/Controllers/TaskController.cs
@@ -1,19 +1,59 @@
-//self
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using TicketingApi.Models;
 
-//Methods
-// Get
-// AllTasks
-// TaskById
-// TasksForProject
+[ApiController]
+[Route("api/[controller]")]
 
-// Post
-// CreateTask
+public class TaskController : Controller {
+    // Add service reference when created
 
-// Put
-// UpdateTask
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TaskModel>>> GetAllTasks() {
+        var tasks = ""; //Service Call
+        return Ok(tasks);
+    }
 
-// Patch
-// PatchTask
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TaskModel>> GetTaskByID(int id) {
+        var task = ""; // Service Call
+        if (task == null) return NotFound();
+        return Ok(task);
+    }
 
-// Delete
-// DeleteTask
+    [HttpPost]
+    public async Task<ActionResult<TaskModel>> CreateTask(TaskModel task) {
+        var newTask = ""; //Service call
+        return CreatedAtAction(nameof(GetTaskByID), new {id=newTask.task_id}, newTask);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateTask(int id, TaskModel task) {
+        if (id != task.task_id) return BadRequest();
+
+        // Service Call
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PatchTask(int id, [FromBody] JsonPatchDocument<TaskModel> patchDocument) {
+        if (patchDocument == null) return BadRequest();
+
+        var task = ""; // Service Call
+        if (task == null) return NotFound();
+
+        patchDocument.ApplyTo(task, ModelState);
+
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        // Service Call
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTask(int id) {
+        //Service Call
+        return NoContent();
+    }
+
+}
--- TicketingApi/Controllers/UserController.cs
+++ TicketingApi/Controllers/UserController.cs
@@ -1,19 +1,65 @@
-//self
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using TicketingApi.Models;
 
-//Methods
-// Get
-// AllUsers
-// UserById
-// TasksForUser
+[ApiController]
+[Route("api/[controller]")]
 
-// Post
-// CreateUser
+public class UserController : Controller {
+    // Add service reference when created
 
-// Put
-// UpdateUser
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserModel>>> GetAllUsers() {
+        var users = ""; //Service Call
+        return Ok(users);
+    }
 
-// Patch
-// PatchUser
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserModel>> GetUserByID(int id) {
+        var user = ""; // Service Call
+        if (user == null) return NotFound();
+        return Ok(user);
+    }
 
-// Delete
-// DeleteUser
+    [HttpGet("{userID}/tasks")]
+    public async Task<ActionResult<IEnumerable<Task>>> GetTasksForUser(int userID) {
+        var tasks = ""; // Service Call
+        return Ok(tasks);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserModel>> CreateUser(UserModel user) {
+        var newUser = ""; //Service call
+        return CreatedAtAction(nameof(GetUserByID), new {id=newUser.user_id}, newUser);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, UserModel user) {
+        if (id != user.user_id) return BadRequest();
+
+        // Service Call
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PatchUser(int id, [FromBody] JsonPatchDocument<UserModel> patchDocument) {
+        if (patchDocument == null) return BadRequest();
+
+        var user = ""; // Service Call
+        if (user == null) return NotFound();
+
+        patchDocument.ApplyTo(user, ModelState);
+
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        // Service Call
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id) {
+        //Service Call
+        return NoContent();
+    }
+
+}
