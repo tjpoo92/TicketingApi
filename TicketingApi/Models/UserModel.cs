d41cd495bd9946afb3487bacaa20be37cc836cd6
Tyler Pooschke <tjpoo92@gmail.com>
2024-11-13 20:34:48 -0600

    updated models to include enums and FKs

--- TicketingApi/Models/UserModel.cs
+++ TicketingApi/Models/UserModel.cs
@@ -2,9 +2,9 @@
 
 public class UserModel
 {
-	public int user_id { get; set; }
-	public string user_name { get; set; }
-	public string user_email { get; set; }
-	public DateTime created_at { get; set; }
-	public DateTime updated_at { get; set; }
+	public int UserId { get; set; }
+	public string UserName { get; set; }
+	public string UserEmail { get; set; }
+	public DateTime CreatedAt { get; set; }
+	public DateTime UpdatedAt { get; set; }
 }
