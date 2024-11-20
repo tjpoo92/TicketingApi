ed00221d9dc49cf29f4b3fdc6a093335c4396057
Tyler Pooschke <tjpoo92@gmail.com>
2024-11-19 21:54:05 -0600

    Created data access class to inject into repository classes

---
+++ TicketingApi/DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace TicketingApi
+{
+	public class DataAccess
+	{
+		public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionString)
+		{
+			using IDbConnection connection = new SqlConnection(connectionString);
+			List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+			return rows;
+		}
+
+		public void SaveData<T>(string sqlStatement, T parameters, string connectionString)
+		{
+			using IDbConnection connection = new SqlConnection(connectionString);
+			connection.Execute(sqlStatement, parameters);
+		}
+	}
+}
