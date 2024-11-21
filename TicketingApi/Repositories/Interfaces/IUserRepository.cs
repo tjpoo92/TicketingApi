f0e4c4d9d1486c43b7553feeb3b46408f8dc7a48
Tyler Pooschke <tjpoo92@gmail.com>
2024-11-20 21:46:04 -0600

    completed nearly all of UserRepo excluding GetTasksByUserIdAsync, updated DataAccess and IUserRepo

--- TicketingApi/Repositories/Interfaces/IUserRepository.cs
+++ TicketingApi/Repositories/Interfaces/IUserRepository.cs
@@ -5,7 +5,7 @@ public interface IUserRepository
     Task<IEnumerable<UserModel>> GetAllUsersAsync();
     Task<UserModel> GetUserByIdAsync(int id);
     Task<IEnumerable<TaskModel>> GetTasksByUserIdAsync(int userID);
-    Task<UserModel> CreateUserAsync(UserModel user);
+    Task CreateUserAsync(UserModel user);
     Task UpdateUserAsync(UserModel user);
     Task DeleteUserAsync(int id);
 }
--- TicketingApi/Repositories/UserRepository.cs
+++ TicketingApi/Repositories/UserRepository.cs
@@ -1,34 +1,65 @@
+using TicketingApi;
 using TicketingApi.Models;
 
 public class UserRepository : IUserRepository
 {
-    public Task<IEnumerable<UserModel>> GetAllUsersAsync()
+    private readonly string _connectionString;
+    private SqlDataAccess db = new SqlDataAccess();
+
+    public UserRepository(string connectionString)
     {
-        throw new NotImplementedException();
+        _connectionString = connectionString;
     }
 
-    public Task<UserModel> GetUserByIdAsync(int id)
+    public async Task<IEnumerable<UserModel>> GetAllUsersAsync()
     {
-        throw new NotImplementedException();
+        string sql = "SELECT * FROM dbo.users";
+
+		return await db.LoadDataAsync<UserModel, dynamic>(sql, new { }, _connectionString);
+    }
+
+    public async Task<UserModel> GetUserByIdAsync(int id)
+    {
+        string sql = "SELECT * FROM dbo.users WHERE user_id = @Id";
+        List<UserModel> output = await db.LoadDataAsync<UserModel, dynamic>(sql, new { Id = id }, _connectionString);
+		return output.First();
     }
 
     public Task<IEnumerable<TaskModel>> GetTasksByUserIdAsync(int userID)
     {
         throw new NotImplementedException();
     }
-    
-    public Task<UserModel> CreateUserAsync(UserModel user)
+
+	public async Task CreateUserAsync(UserModel user)
     {
-        throw new NotImplementedException();
+        string sql = "INSERT INTO dbo.users (user_name, user_email) VALUES (@UserName, @UserEmail)";
+
+		await db.SaveDataAsync(sql, new { user.UserName, user.UserEmail }, _connectionString);
     }
 
-    public Task UpdateUserAsync(UserModel user)
+	public async Task UpdateUserAsync(UserModel user)
     {
-        throw new NotImplementedException();
+        string sql = String.Empty;
+        if (user.UserEmail == null)
+        {
+            sql = "UPDATE dbo.users SET user_name = @UserName WHERE user_id = @Id";
+            await db.SaveDataAsync(sql, new { user.UserName, user.UserId }, _connectionString);
+        }
+        else if (user.UserName == null)
+        {
+            sql = "UPDATE dbo.users SET user_email = @UserEmail WHERE user_id = @Id";
+            await db.SaveDataAsync(sql, new { user.UserEmail, user.UserId }, _connectionString);
+		}
+        else
+        {
+			sql = "UPDATE dbo.users SET user_name = @UserName, user_email = @UserEmail WHERE user_id = @Id";
+            await db.SaveDataAsync(sql, new { user.UserName, user.UserEmail }, _connectionString);
+		}
     }
 
-    public Task DeleteUserAsync(int id)
+    public async Task DeleteUserAsync(int id)
     {
-        throw new NotImplementedException();
+        string sql = "DELETE FROM dbo.users WHERE user_id = @Id";
+        await db.SaveDataAsync(sql, new { Id = id }, _connectionString);
     }
 }
